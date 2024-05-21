3dc4ee3725ecd0653fab6d0631cef9433721a287
Cameron Leverett <cameron@ntx.ventures>
2024-05-21 12:30:38 -0700

    added rspec tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+# spec/spec_helper.rb
+RSpec.configure do |config|
+  # Use the specified formatter
+  config.default_formatter = 'doc'
+  # Run all examples in random order
+  config.order = :random
+end
